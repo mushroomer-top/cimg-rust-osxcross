db99883e94578209cfc0d8ddebf30c732caaf088
Tijn <tijn@112batman.com>
2026-08-21 00:51:25 +0200

    fix typo in LD_LIBRARY_PATH

--- with_osxcross.sh
+++ with_osxcross.sh
@@ -6,7 +6,7 @@ export SDKROOT="$OSXCROSS_ROOT/SDK/MacOSX11.1.sdk"
 
 export PATH="$OSXCROSS_ROOT/bin:$PATH"
 
-export LD_LIBRARY_PATH="/home/arch/osxcross/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="$OSXCROSS_ROOT/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
 
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_LINKER="aarch64-apple-darwin20.2-clang"
 export CARGO_TARGET_AARCH64_APPLE_DARWIN_AR="aarch64-apple-darwin20.2-ar"
